0fb9cda2d4b6781bb111fce71520adf77be2eb92
thqnam <141632310+thqnam@users.noreply.github.com>
2026-09-01 13:29:39 +0700

    003

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,7 +1,7 @@
 import { isRoomLabel } from "@/app/utils/room-label";
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
