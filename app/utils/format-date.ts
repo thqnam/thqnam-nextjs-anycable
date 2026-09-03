62d0270c3430610ac74f90553a60fb4de3c5c987
thqnam <141632310+thqnam@users.noreply.github.com>
2026-09-03 07:24:06 +0700

    032

--- app/utils/format-date.ts
+++ app/utils/format-date.ts
@@ -2,6 +2,7 @@ export function formatDateToHours(dateString: string) {
   const date = new Date(dateString);
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  return `${hours}:${minutes}`;
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  
+  return `${hours}:${minutes}:${seconds}`;
 }
